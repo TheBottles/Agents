3c02e34f6bdf02e51e65062e63d16ab04390c5a8
jennifersalas <jennysalas@me.com>
2018-05-14 11:19:36 -0700

    Added some files to begin structuring our bot.

---
+++ splitter.py
@@ -0,0 +1,10 @@
+"""
+The goal of the splitter script is to find the way of splitting up a set of
+units for optimal flanking (minimize health loss, maximize damage). Identify
+which group will be head on, and which will be flanking.
+"""
+
+# Important things to note:
+#   1. You may want use a class to perform these tasks
+#   2. You may want to have a class load up pre-configured weights
+#   3. You may want it to dynamically learn everytime it makes a decision
