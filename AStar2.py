d17c47c5487e298e234a5562e91b2ffc933dd727
ErickCruz1031 <erickcruz1031@gmail.com>
2018-06-03 13:35:17 -0700

    New implementation for pathplanning

--- AStar2.py
+++ AStar2.py
@@ -109,19 +109,79 @@ def circleEquation(r, Cx, Cy, Point):
 
     #Maybe this could be the cost 
     Diff  = r - Total 
-    return Diff 
+    return Diff
 
 
-'''A* implementation using the class slides'''
 def pathfinding(obs, Start, Goal):
+    d = 15 
+    T = []
+    D = []
+    Waypoint = []
+    Flankpoint = []
 
-    #Now we do all the agorithm here
-    #Not Implemented, just teting with it 
     Circle_X = int((Start[0] + Goal[0])/2)
     Circle_Y = int((Start[1] + Goal[1])/2)
     temp1 = math.pow((Start[0] - Goal[0]),2)
     temp2 = math.pow((Start[1] - Goal[1]),2)
     radius = math.sqrt(temp1 + temp2)
+    slope = (Start[1] - Goal[1])/(Start[0] - Goal[0])
+
+    xd = (d/(math.sqrt(1 + (slope*slope))) + Goal[0])
+    yd = ((d*slope)/(math.sqrt(1 + (slope*slope)))) + Goal[1]
+
+    D.append(xd) 
+    D.append(yd)
+
+
+    xt = (- d/(math.sqrt(1 + (slope*slope))) + Goal[0])
+    yt = (- (d*slope)/(math.sqrt(1 + (slope*slope)))) + Goal[1]
+
+    T.append(xt)
+    T.append(yt) 
+
+
+    if Distance_Calc(Start, D) < Distance_Calc(Start, Goal):
+        Waypoint.append(D[0])
+        Waypoint.append(D[1])
+        Flankpoint.append(T[0])
+        Flankpoint.append(T[1])
+    elif Distance_Calc(Start, Goal) < Distance_Calc(Start, D):
+        Waypoint.append(T[0])
+        Waypoint.append(T[1])
+        Flankpoint.append(D[0])
+        Flankpoint.append(D[1])
+
+    distancew = Distance_Calc(Goal, Waypoint)
+    distancet = Distance_Calc(Goal, Flankpoint)
+    print("Distance to waypoint is", distancew)
+    print("Other distance to flankpoint is ", distancet)
+
+
+
+    print("WE ARE IN PATH FINDING")
+    print("We are at ", Start)
+    print("They are at ", Goal)
+    print("Slope is ", slope)
+    #print("We are going to", xd, " and ", yd)
+    #print("Middle of the circle is at ", Circle_X, " and ", Circle_Y)
+    #print("The other is at ", xt, " and ", yt)
+    print("Waypoint is ", Waypoint)
+    print("Flankpoint is ", Flankpoint)
+
+
+
+
+
+
+
+
+
+'''A* implementation using the class slides'''
+def AStar_Algo(obs, Start, Goal):
+
+    #Now we do all the agorithm here
+    #Not Implemented, just teting with it 
+    
     #Circle_Y/X are the coordinates for the middle of the circle
     #Now we can use the formula to see how far off each of these points are from the circle 
     
@@ -162,8 +222,8 @@ def pathfinding(obs, Start, Goal):
             Neighbors = Graph(Current.state, shape)
             for n in Neighbors:
                 #There's 3 cases but only took accound for 2 so far
-                D = circleEquation(radius, Circle_X, Circle_Y, n)
-                print("The difference is ", D)
+                
+                #print("This is the shape ", shape)
                 Temp = StateObject() #Create the StateObj for heap
                 #Distance from the goal + cost of moving one square
                 # fn = Distance_Calc(Goal[0], Goal[1], n[0], n[1]) + 1
@@ -205,13 +265,19 @@ We then make a dictionary that represents the "graph" that is the map.
 Each key in the dictionary is a tuple that represents a point and the value
 for each key is a tuple of tuples that represents all the other points
 that the current key connects to.'''
-def A_Star(obs, location, target):
+def A_Star(obs, location, target, flank = False):
     #Account for the negative parts of the graph
     '''For the sake of implementation I started with 0,0
     but this will change based on where in the map we are
     since we are not always going to be at (0,0)'''
-
-    return pathfinding(obs,location, target)
+    '''
+    if flank:
+        return pathfinding(obs, location, target)
+    else:
+        return AStar_Algo(obs,location, target)
+    '''
+    pathfinding(obs, location, target)
+    return AStar_Algo(obs,location, target)
 
 
 
